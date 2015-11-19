15e9a57deedbd2e8c8a2e626cc18f3e051679484
nickeljew <apple@Nickel-De-MBP.local>
2015-11-19 13:31:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# ES6-DOM
+
+Simple DOM manipulation object for modern browser using plain javascript and ES6 module syntax
+
+
+## Installation
+
+	npm install es6-dom --save
+
+## Example
+
+    <div id="item1">Hello World</div>
+    
+
+    import Dom from 'es6-dom'
+    
+    let el = Dom.nodeById(id)
+    Dom.addClass(el, "info-list")
+    
+
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,106 @@
+
+let dom = {
+
+    isDescendant(parent, child) {
+        let node = child.parentNode
+
+        while (node !== null) {
+            if (node === parent) return true
+            node = node.parentNode
+        }
+
+        return false;
+    },
+
+    offset(el) {
+        let rect = el.getBoundingClientRect()
+        return {
+            top: rect.top + document.body.scrollTop,
+            left: rect.left + document.body.scrollLeft,
+        }
+    },
+
+    getStyleAttributeAsNumber(el, attr) {
+        let attrStyle = el.style[attr];
+        let attrNum = 0;
+        if (attrStyle && attrStyle.length) {
+            attrNum = parseInt(attrStyle)
+        }
+
+        return attrNum
+    },
+
+    addClass(el, className) {
+        if (el.classList)
+            el.classList.add(className)
+        else
+            el.className += ' ' + className
+    },
+
+    removeClass(el, className) {
+        if (el.classList)
+            el.classList.remove(className)
+        else
+            el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ')
+    },
+
+    hasClass(el, className) {
+        if (el.classList)
+            return el.classList.contains(className)
+        else
+            return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className)
+    },
+
+    toggleClass(el, className) {
+        if (this.hasClass(el, className))
+            this.removeClass(el, className)
+        else
+            this.addClass(el, className)
+    },
+
+    forceRedraw(el) {
+        let originalDisplay = el.style.display
+
+        el.style.display = 'none'
+        el.offsetHeight
+        el.style.display = originalDisplay
+    },
+
+    withoutTransition(el, callback) {
+        let originalTransition = el.style.transition
+
+        //turn off transition
+        el.style.transition = null
+
+        callback()
+
+        //force a redraw
+        this.forceRedraw(el)
+
+        //put the transition back
+        el.style.transition = originalTransition
+    },
+
+    nodeById(id) {
+        return document.getElementById(id)
+    },
+
+    nodeBySelector(el, s) {
+        return (el || document).querySelector(s)
+    },
+
+    nodesBySelector(el, s) {
+        return (el || document).querySelectorAll(s)
+    },
+
+    text(el, text) {
+        if (typeof text === 'string') {
+            el && (el.innerText = text)
+            return this
+        }
+        return el ? (el.innerText || el.textContent || '') : ''
+    },
+
+}
+
+export default dom
